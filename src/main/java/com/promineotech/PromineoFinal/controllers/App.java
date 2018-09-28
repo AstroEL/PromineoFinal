8a9c390512118b863155d98a292080bb662ecd3c
keith <kmacdadie@example.com>
2018-09-27 19:52:23 -0700

    upload

---
+++ src/main/java/com/promineotech/PromineoFinal/controllers/App.java
@@ -0,0 +1,13 @@
+package com.promineotech.PromineoFinal.controllers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan("com.promineotech.mvnPromineoFinal")
+@SpringBootApplication
+public class App {
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+}
