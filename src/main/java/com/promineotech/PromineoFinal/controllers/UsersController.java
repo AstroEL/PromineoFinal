3c9cdd7df5a967e70b215f4feafba4925c31562e
Nick Suwyn <nicholas_suwyn@suwynitsolutions.com>
2018-10-04 20:46:03 -0700

    trying to fix classpath issues

--- src/main/java/com/promineotech/PromineoFinal/controllers/UsersController.java
+++ src/main/java/com/promineotech/PromineoFinal/controllers/UsersController.java
@@ -1,4 +1,8 @@
+package com.promineotech.PromineoFinal.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UsersController {
